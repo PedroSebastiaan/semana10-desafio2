e196fdcb3b69f180abe6a8577013c209172f3950
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-17 22:49:16 -0400

    fix: name images folder

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'image')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'sass')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'webfonts')
 
